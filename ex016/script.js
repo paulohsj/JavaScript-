0fd6785ba49eddbf88716ccdc46de563556e29ba
Paulo Henrique Da Silva Júnior <paulohsj44@gmail.com>
2022-11-22 22:47:41 -0300

    Resolvendo exercicios JS

---
+++ ex016/script.js
@@ -0,0 +1,27 @@
+function contar() {
+    var ini = document.getElementById('txtini')
+    var fim = document.getElementById('txtfim')
+    var passo = document.getElementById('txtpas')
+    if (passo.value.length == 0 || fim.value.length == 0) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+            for (ini; ini <= fim; ini++) {
+            res.innerHTML = `Resultado contado foi `
+        }
+    }
+
+
+
+
+    //se, inicio menor ou igual ao fim soma mais 1 e testa dnv
+    //se não (ou seja, maior que o fim) fim da logica
+
+
+
+
+
+
+
+
+
+}
